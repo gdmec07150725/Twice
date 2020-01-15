d6098499282e0f913556778bd9c17843167a215e
Tony <715296475@qq.com>
2020-01-15 22:11:39 +0800

    feat(layout): 新增layout文件

---
+++ src/pages/layouts/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    HHAH
+  </div>
+</template>
+
+<script>
+export default {
+  name: "layout",
+  data() {
+    return {
+      index: 1
+    };
+  },
+  methods: {
+    handleCollease() {
+      console.log(1);
+    }
+  }
+};
+</script>
